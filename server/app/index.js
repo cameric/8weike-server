9ef88c438e551a69049da0a476a274ec3c287684
zhzhang2012 <zhzhangtony@gmail.com>
2016-07-31 16:39:29 -0700

    [Server] Integrate Webpack and Babel into the build

--- server/app/index.js
+++ server/app/index.js
@@ -1,22 +1,29 @@
 'use strict';
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser'),
+      cookieParser = require('cookie-parser'),
+      express = require('express'),
+      path = require('path');
 
-const config = require('./config/config');
-const Router = require('./api/router');
+const config = require('./config/config'),
+      Router = require('./api/router'),
+      webpack = require('webpack'),
+      webpackConfig = require('../webpack.config');
 
 // Express server
 const app = express();
+const bundler = webpack(webpackConfig);
 
 // Set up view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(config.root, config.webapp.folder));
 
-// Serve static files
+// Serve static files and compiled js
 app.use(express.static(path.join(config.root, config.webapp.folder)));
+app.use(require('webpack-dev-middleware')(bundler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+}));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
