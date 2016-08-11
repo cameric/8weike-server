c0a0e5834932b6bc9193043987b05b993d79743f
Spencer Michaels <sxmichaels@gmail.com>
2016-08-11 01:22:51 -0500

    Reverted Harmony JSX changes.

--- server/webapp/shared/routes.js
+++ server/webapp/shared/routes.js
@@ -1,7 +1,8 @@
 // This file defines all client routes
 
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import { Route, IndexRoute } from 'react-router';
+
 import DocumentTitle from 'react-document-title';
 import IndexPage from '../pages/index_page';
 import LoginPage from '../pages/login_page';
@@ -9,27 +10,18 @@ import LoginPage from '../pages/login_page';
 // App component is a top-level virtual container that
 // doesn't belong to any page.
 class App extends React.Component {
-  render() {
-    return (
-        < DocumentTitle
-    title = "8WeiKe" >
-        {this.props.children
-  }
-  </
-    DocumentTitle >
-  )
-  }
+    render() {
+        return (
+            <DocumentTitle title="8WeiKe">
+                { this.props.children }
+            </DocumentTitle>
+        )
+    }
 }
 
 export default (
-< Route
-path = "/"
-component = {App} >
-    < IndexRoute
-component = {IndexPage} / >
-    < Route
-path = "login"
-component = {LoginPage} / >
-    < / Route >
-)
-;
+    <Route path="/" component={App}>
+        <IndexRoute component={IndexPage}/>
+        <Route path="login" component={LoginPage}/>
+    </Route>
+);
