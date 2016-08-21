b84c4955fc00cfac10a17d284c075aa1ef488b3b
zhzhang2012 <zhzhangtony@gmail.com>
2016-08-21 19:15:28 -0400

    Add env var to front-end. Fix various tools that only show in sev

--- server/webapp/pages/components/login-page.js
+++ server/webapp/pages/components/login-page.js
@@ -8,9 +8,6 @@ export default class LoginPage extends React.Component {
     return (
       <DocumentTitle title="8WeiKe - Login">
         <div>This is the login page of 8weike website.</div>
-        <span>电话 </span><input type="text" value={this.state.phone} />
-        <span>密码 </span><input type="password" value={this.state.password} />
-        <button onClick={this.props.handleSendTFACode}>登陆</button>
       </DocumentTitle>
     )
   }
