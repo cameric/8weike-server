e2e0d399ec03762d621462a64727d4f1c239a36d
Spencer Michaels <sxmichaels@gmail.com>
2016-08-27 13:46:39 -0500

    post-review bugfixes

--- server/app/controllers/auth/login.js
+++ server/app/controllers/auth/login.js
@@ -1,6 +1,6 @@
-function loginWithPhone(req, res) {
-  if (!req.user) return res.redirect('/api/login/phone');
-  return res.redirect(`/api/user/${req.user.id}`);
+function loginWithPhone(req, res, next) {
+  if (!req.user) next(new Promise.OperationalError('req.user is null'));
+  return res.status(200).send(JSON.stringify({ id: req.user.id }));
 }
 
 function loginWithWeixin(req, res) {
--- server/app/controllers/auth/signup.js
+++ server/app/controllers/auth/signup.js
@@ -4,9 +4,6 @@ function signupWithPhone(req, res, next) {
   const { phone, password } = req.body;
   userModel.signupWithPhone(phone, password)
       .then((data) => { res.status(200).send({ data }); })
-      // TODO: We probably need to respond with some data about the cause of recoverable errors, but
-      // we also shouldn't just return the error itself --- that's a security risk.
-      .error((_) => { res.status(400).send(); })
       .catch(next);
 }
 
