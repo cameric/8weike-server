357a0ba9a5444689a52353360a5b3133b96f0107
zhzhang2012 <zhzhangtony@gmail.com>
2016-11-04 04:19:46 -0700

    Fix style

--- server/webpack.client.config.js
+++ server/webpack.client.config.js
@@ -4,10 +4,9 @@ const generateConfig = require('./webpack.client.config.template');
 
 // Front-end locale mappings
 const languages = {
-  "zh-CN": require('./webapp/locales/zh-CN.json'),
+  'zh-CN': require('./webapp/locales/zh-CN.json'),
   en: null,
 };
 
-module.exports = Object.keys(languages).map((langName) => {
-  return generateConfig(langName, languages[langName]);
-});
+module.exports = Object.keys(languages).map((langName) =>
+    generateConfig(langName, languages[langName]));
